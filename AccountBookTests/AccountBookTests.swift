951455b2395676e05eaefae91cd3f93ff4e265d2
kimdongjune <jun36817@naver.com>
2022-05-14 18:47:21 +0900

    [Feature] - Test 설정 완료.

---
+++ AccountBookTests/AccountBookTests.swift
@@ -0,0 +1,14 @@
+//
+//  AccountBookTests.swift
+//  AccountBookTests
+//
+//  Created by 김동준 on 2022/05/14.
+//
+
+import XCTest
+@testable import AccountBook
+
+class AccountBookTests: XCTestCase {
+
+
+}
